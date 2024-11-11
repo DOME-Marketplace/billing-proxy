7dc7a561c213fb613d30996977ebd80b15419a04
pasquy <pasquale.vitale@gmail.com>
2024-11-11 14:44:11 +0100

    Update tests

--- src/main/java/it/eng/dome/billing/proxy/controller/ServiceAdminController.java
+++ src/main/java/it/eng/dome/billing/proxy/controller/ServiceAdminController.java
@@ -1,14 +1,24 @@
 package it.eng.dome.billing.proxy.controller;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/admin")
@@ -16,24 +26,31 @@ public class ServiceAdminController {
 
 	private static final Logger log = LoggerFactory.getLogger(ServiceAdminController.class);
 
-    @Value("${application.name}")
-    private String appName;
+    @Autowired
+    private BuildProperties buildProperties;
 
-    @Value("${build.version}")
-    private String buildVersion;
-
-    @Value("${build.timestamp}")
-    private String buildTimestamp;
-
-    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
+    @Operation(responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\"name\":\"Billing Proxy\", \"version\":\"0.0.1\", \"release_time\":\"11-11-2024 14:40:33\"}")
+                ))
+        })
     public Map<String, String> getInfo() {
         log.info("Request getInfo");
         Map<String, String> map = new HashMap<String, String>();
-        map.put("name", appName);
-        map.put("version", buildVersion);
-        map.put("timestamp", buildTimestamp);
+        map.put("version", buildProperties.getVersion());
+        map.put("name", buildProperties.getName());
+        map.put("release_time", getFormatterTimestamp(buildProperties.getTime()));
         log.debug(map.toString());
         return map;
+    }
+	
+    private String getFormatterTimestamp(Instant time) {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        ZonedDateTime zonedDateTime = time.atZone(ZoneId.of("Europe/Rome"));
+    	return zonedDateTime.format(formatter);
     }
 
 }
--- src/test/java/it/eng/dome/billing/proxy/BillingProxyApplicationTests.java
+++ src/test/java/it/eng/dome/billing/proxy/BillingProxyApplicationTests.java
@@ -1,13 +1,38 @@
 package it.eng.dome.billing.proxy;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BillingProxyApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @Test
+    public void shouldReturnExpectedMessage() throws Exception {
+
+        mockMvc.perform(get("/admin/info").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.version").value(buildProperties.getVersion()))
+            .andExpect(jsonPath("$.name").value(buildProperties.getName()));
+    }
 
 }
